7498d5d4a940e2d9d659e4e607b22e3241a7c953
jonestoddcm <Charlotte.JonesTodd@niwa.co.nz>
2018-12-21 15:55:40 +1300

    adding new messages

--- inst/sep.sh
+++ inst/sep.sh
@@ -2,7 +2,7 @@
 
 infile=$1
 num=(0 30 48 72 94 120 168 216 240 280 328)
-letter=(a m_e h e r e_e b s l n y_e)
+letter=(a m_e h e r e_e b s l n y)
 for ((i=0; i<11; i+=1));
 do
 convert $infile -crop 24X44+${num[i]}+0 ../alphabet/${letter[i]}.png
